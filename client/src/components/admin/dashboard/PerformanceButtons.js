e25a56ea37865778acb42a05d417ee7fe32f5844
Jimmel Ibo <jimmel.ibo1998@yahoo.com>
2019-11-27 16:20:26 +0800

    test change

--- client/src/components/admin/dashboard/PerformanceButtons.js
+++ client/src/components/admin/dashboard/PerformanceButtons.js
@@ -5,7 +5,9 @@ class PerformanceButtons extends Component {
     return (
       <Fragment>
         <div className="row">
-          <h3 className="center light-green-text text-darken-3">Performance Jimmel Pogi</h3>
+          <h3 className="center light-green-text text-darken-3">
+            Performance Tizon
+          </h3>
           <div className="col s12 m4">
             <a
               className="waves-effect waves-light btn perf-btn light-green darken-1"
